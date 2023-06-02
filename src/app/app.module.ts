43efb92c79a03fb7a47140a48ff570ef3fb9ee0c
Sheik Junaid <P7167254@nessinhyd.com>
2023-06-02 10:09:51 +0530

    'capstock'

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,29 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http'
 
+import { CanvasJSAngularChartsModule } from '@canvasjs/angular-charts';
+import { HomeComponent } from './home/home.component';
+import { DetailsComponent } from './details/details.component';
+import { NgxPaginationModule } from 'ngx-pagination';
+
+import { SearchPipe } from './pipes/search.pipe';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    DetailsComponent,
+    SearchPipe
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    CanvasJSAngularChartsModule,
+    NgxPaginationModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/models/stock.model.ts
@@ -0,0 +1,10 @@
+export class Stock{
+    stockId:string=''
+    stockName:string='';
+    currentPrice:number=0;
+    prevPrice:number=0;
+    ltp:number=0;
+    volume:number=0;
+    date=new Date()
+    
+}
